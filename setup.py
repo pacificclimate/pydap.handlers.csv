1ace73675e3038e38ec588ff916c8c46c0d76e9a
Roberto De Almeida <roberto@dealmeida.net>
2012-07-03 00:13:15 -0300

    Initial version of a new CSV handler.

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.3'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='pydap.handlers.csv',
+    version=version,
+    description="A handler that allows Pydap to server CSV files.",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='csv opendap pydap dap data access',
+    author='Roberto De Almeida',
+    author_email='roberto@dealmeida.net',
+    url='http://pydap.org/handlers.html#csv',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    namespace_packages = ['pydap', 'pydap.handlers'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['pydap.handlers.csv=pydap.handlers.csv:main']
+    }
+)
